d0b3b25c0c52b45dc470b061487371e4f314c4c3
webmind.pt <webmind.pt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-03 20:55:31 +0000

    Update to version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/wm-jqmath/trunk@1212072 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wm_jqmath.php
+++ wm_jqmath.php
@@ -4,16 +4,15 @@ Plugin Name: WM jqMath
 Plugin URI: http://webmind.pt/portfolio/wm-jqmath/
 Description: Create math formulas on your posts and pages using jqMath from MathScribe
 Author: Carlos Santos, webmind.pt
-Version: 1.0
+Version: 1.1
 Author URI: http://www.webmind.pt
 License: MIT
 */
 
 // Add style and scripts
 function wmjqmath_enqueued_assets() {
-    wp_enqueue_style( 'jqmath-css', plugin_dir_url( __FILE__ ) . 'css/jqmath-0.4.0.css' );
-    wp_enqueue_script( 'jqmath-script', plugin_dir_url( __FILE__ ) . 'js/jqmath-etc-0.4.2.min.js', array( 'jquery' ), '1.0', false );
-    wp_enqueue_script( 'jscurry-script', plugin_dir_url( __FILE__ ) . 'js/jscurry-0.3.0.min.js', array( 'jquery' ), '1.0', false );
+    wp_enqueue_style( 'jqmath-css', plugin_dir_url( __FILE__ ) . 'css/jqmath-0.4.3.css' );
+    wp_enqueue_script( 'jqmath-script', plugin_dir_url( __FILE__ ) . 'js/jqmath-etc-0.4.3.min.js', array( 'jquery' ), '0.4.3', false );
 }
 
 // Generate jqMath formula from shortcode
